1eb76a012178525962b61dea5593a2c421fcea89
Anazodo Brendan Ifechukwu <bifechukwu4@gmail.com>
2022-06-02 14:43:14 -0700

    A C program that prints with puts instead of prntif

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,31 @@
+/*
+ *
+ *  * 4-puts.c
+ *
+ *   *  A program that prints with puts and not printf
+ *
+ *    */
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - My entry point for my program
+ *
+ *   *
+ *
+ *    * Return: returns 0
+ *
+ *     */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
