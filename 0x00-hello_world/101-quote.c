ff6d49727830b499ef2ae71d4fd344ab617e94fd
Anazodo Brendan Ifechukwu <bifechukwu4@gmail.com>
2022-06-04 14:49:18 -0700

    A C program that prints exactly and that piece of art is useful

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,29 +1,16 @@
 /*
  * 101-quote.c
  *  A program that prints piece of art is useful\" - Dora Korpar, 2015-10-19
- *
  */
-
 #include <stdio.h>
 
-
-
  /**
-  *
   *  * main - My entry point for my program
   *
-  *   *
-  *
   *    * Return: returns 1
-  *
   */
-
- int main(void)
-
+int main(void)
 {
-
 		printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-
 			return (1);
-
 }
