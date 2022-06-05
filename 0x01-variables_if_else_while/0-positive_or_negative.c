a85a035474e59894991f138e12b2dedef23e4796
Anazodo Brendan Ifechukwu <bifechukwu4@gmail.com>
2022-06-05 14:51:01 -0700

    A program will assign a random number to the variable n each time it is executed.

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,47 @@
+/*
+ *
+ * 0-positive_or_negative.c*
+ *
+ * A program that generate a random number
+ * And checks whether the number is positive or negative.
+ *
+ * Auth: SErenscott
+ */
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ * main - The entry point of the program
+ * Return: returns 0
+ */
+
+int main(void)
+
+{
+
+	int n;
+
+
+
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
+	return (0);
+}
