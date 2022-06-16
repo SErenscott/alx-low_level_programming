66654794f91d5d972320745b0e51cbccebfee4ee
Anazodo Brendan Ifechukwu <bifechukwu4@gmail.com>
2022-06-16 14:13:36 -0700

    Modified task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
  * print_last_digit - print last digit of number
- * @a: number to print last digit of
+ * @a: number to print last digit of number
  * Return: thlast digit of a
  */
 int print_last_digit(int a)
