dd218902a48c08b37c5a80245621f52429373797
Anazodo Brendan Ifechukwu <bifechukwu4@gmail.com>
2022-06-16 12:56:28 -0700

    Task 5 completed

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - checks if a number is
+ * negative
+ * positive
+ * zero
+ * @n: The number to check
+ * Return: 0 if zero if negative 1 positive -1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
